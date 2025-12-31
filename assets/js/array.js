a75c46692a058e1e92738bdc2ffa7a48ae18f567
Maksym <praniknumberone@gmail.com>
2025-12-31 04:32:29 +0100

    DoneTask5

---
+++ assets/js/array.js
@@ -0,0 +1,6 @@
+console.log("\n--- ЗАВДАННЯ 5  ---");
+const isEqual = (str1, str2) => str1.toLowerCase() === str2.toLowerCase();
+
+console.log(isEqual("pApA", "papa"));
+console.log(isEqual("qwerty", "QWErty"));
+console.log(isEqual("aaa", "EEE"));

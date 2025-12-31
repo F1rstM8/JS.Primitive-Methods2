28edd3b409d011e73ad4f5b44704c90125d3b559
Maksym <praniknumberone@gmail.com>
2025-12-31 04:28:34 +0100

    DoneTask2

---
+++ assets/js/array.js
@@ -0,0 +1,6 @@
+console.log("--- ЗАВДАННЯ 2 ---");
+const getIntegerPart = (number) => Math.trunc(number);
+
+console.log(getIntegerPart(5.95)); 
+console.log(getIntegerPart(-3.4)); 
+console.log(getIntegerPart(10));   
